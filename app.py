136906ec9f9c8db122a32dfd45dadb4b449d498c
deysonduarte <claudiowaldeysonduarte@gmail.com>
2025-02-19 19:29:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def feed():
     xml_url = "https://www.hinode.com.br/XMLData/consultores.xml"
+    response = requests.get(xml_url)
 
-    try:
-        response = requests.get(xml_url, headers={"User-Agent": "Mozilla/5.0"})
-
-        if response.status_code == 200:
-            return Response(response.content, mimetype='application/xml')
-        else:
-            return f"Erro ao buscar XML: {response.status_code}", 500
-    except Exception as e:
-        return f"Erro inesperado: {str(e)}", 500
+    if response.status_code == 200:
+        return Response(response.content, mimetype='application/xml')
+    else:
+        return "Erro ao buscar o XML", 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
